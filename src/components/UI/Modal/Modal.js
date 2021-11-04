8342fbcf9bfac0f5e099cb2282a1a7935fce9541
iuliamoldovan21 <iulliaa266@gmail.com>
2021-11-04 12:38:16 +0200

    modal moved in dom with portals

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import styles from "./Modal.module.css";
-import React from "react";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.closeModal} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button type="button" onClick={props.closeModal}>
+          Okay
+        </Button>
+      </footer>
+    </Card>
+  );
+};
 
 const Modal = (props) => {
   return (
     <React.Fragment>
-      <div className={styles.backdrop} onClick={props.closeModal} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button type="button" onClick={props.closeModal}>
-            Okay
-          </Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop closeModal={props.closeModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          closeModal={props.closeModal}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </React.Fragment>
   );
 };
